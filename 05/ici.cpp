8aef131c7496fb0d3a2da1ef27eeaa2471882eed
lukmi15 <lukas.mirow@web.de>
2019-12-27 17:40:07 +0100

    Working on day 5 part 1 & fixed ugly error echoing in Makefile

--- 05/ici.cpp
+++ 05/ici.cpp
@@ -10,6 +10,8 @@ Date of creation	: 12/09/2019
 #include <iostream>
 #include <sstream>
 #include <cstring>
+#include <vector>
+#include <cmath>
 using namespace std;
 
 void Intcode_interpreter::load_program(const ICI_TYPE *program, const unsigned length)
@@ -20,19 +22,37 @@ void Intcode_interpreter::load_program(const ICI_TYPE *program, const unsigned l
 	memcpy(memory, program, memory_size);
 }
 
+vector<bool> Intcode_interpreter::get_parameter_modes(const unsigned parameter_count, const ICI_TYPE instruction)
+{
+	vector<bool> pms;
+	unsigned parameter_mode;
+	for (unsigned current_parameter_mask = 100; current_parameter_mask < (unsigned)pow(10, parameter_count + 2); current_parameter_mask *= 10)
+	{
+		parameter_mode = (instruction % (current_parameter_mask * 10)) / current_parameter_mask;
+		if (parameter_mode < 0 or parameter_mode > 1)
+		{
+			stringstream ss;
+			ss << "Invalid parameter mode `" << parameter_mode << '`';
+			throw invalid_arguemnt(ss.str());
+		}
+		else
+			pms.push_back(parameter_mode == 1);
+	}
+	return pms;
+}
+
 void Intcode_interpreter::step()
 {
 	unsigned parameter_count, *parameter_modes;
+	vector<bool>parameter_modes;
 	switch (memory[pc] % 100)
 	{
 		case STOP_CODE:
 			pc = -1;
 			return;
 		case ADD_CODE:
-			parameter_count = 4; //TODO: How to properly add parameter mode support?
-			/*ICI_TYPE params[parameter_count-1];
-			for (unsigned i=0; i<parameter_count-1; i++)
-				if (*/
+			parameter_count = 4;
+			parameter_modes = get_parameter_modes(parameter_count, memory[pc]);
 			add(memory[pc + 1], memory[pc + 2], memory[pc + 3]);
 			break;
 		case MUL_CODE:
